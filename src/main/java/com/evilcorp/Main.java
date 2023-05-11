f433146087074d283ce30aa6f8084b5cec1cab89
Ivan Ivanov <lalalal12342@gmail.com>
2023-05-11 12:40:45 +0300

    1

---
+++ src/main/java/com/evilcorp/Main.java
@@ -0,0 +1,11 @@
+package com.evilcorp;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String name = "Денис";
+        System.out.println("Привте, "+name);
+    }
+}
